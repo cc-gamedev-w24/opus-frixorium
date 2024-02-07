7bc9b4c1aad562627bacfcb986b2fe20db6e58a9
Blecaj1982 <90987495+Blecaj1982@users.noreply.github.com>
2024-02-06 19:33:39 -0500

    Added audio manager, test scene

---
+++ OPVS-FRIXORIVM/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioSource _musicSource, _effectsSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void PlaySound(AudioClip clip)
+    {
+        _effectsSource.PlayOneShot(clip);
+    }
+}
