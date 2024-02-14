91670dc204d07f90bf65aea4e31c9e2a2ea6f5cb
Ethan Major <ethanmajor9@gmail.com>
2024-02-13 22:40:46 -0500

    Added cancel button for start countdown

--- OPVS-FRIXORIVM/Assets/Scripts/Lobby/LobbyUIManager.cs
+++ OPVS-FRIXORIVM/Assets/Scripts/Lobby/LobbyUIManager.cs
@@ -1,34 +1,53 @@
 using System.Collections;
 using System.Globalization;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-public class LobbyUIManager : MonoBehaviour
+namespace Lobby
 {
+    public class LobbyUIManager : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text countdownText;
+        [SerializeField] private GameObject countdownPanel;
+        [SerializeField] private float countdownTimer = 3.0f;
 
-   [SerializeField] private TMP_Text countdownText;
-   [SerializeField] private GameObject countdownPanel;
-   
-   private float _countdownTimer = 3.0f;
+        private bool _cancelled;
+        private Coroutine _countdownCoroutine;
 
-   public void Awake()
-   {
-      countdownPanel.SetActive(false);
-   }
-   
-   public void OnStartClick()
-   {
-      countdownPanel.SetActive(true);
-      StartCoroutine(StartCountdown());
-   }
+        public void Awake()
+        {
+            countdownPanel.SetActive(false);
+            _cancelled = false;
+        }
 
-   private IEnumerator StartCountdown()
-   {
-      while (_countdownTimer > 0)
-      {
-         yield return new WaitForSeconds(1);
-         _countdownTimer--;
-         countdownText.text = _countdownTimer.ToString(CultureInfo.InvariantCulture);
-      }
-   }
+        public void OnStartClick()
+        {
+            countdownPanel.SetActive(true);
+            _cancelled = false; 
+        
+            if (_countdownCoroutine != null)
+                StopCoroutine(_countdownCoroutine);
+            _countdownCoroutine = StartCoroutine(StartCountdown());
+        }
+
+        private IEnumerator StartCountdown()
+        {
+            var countdown = countdownTimer;
+            while (countdown > 0 && !_cancelled)
+            {
+                countdownText.text = countdown.ToString(CultureInfo.InvariantCulture);
+                yield return new WaitForSeconds(1);
+                countdown--;
+            }
+
+            countdownPanel.SetActive(false); 
+            _cancelled = false;
+        }
+
+        public void OnCancelClick()
+        {
+            _cancelled = true;
+            countdownPanel.SetActive(false); 
+        }
+    }
 }
