07a6074b7137378cb584dd3f5a31868faeea16da
Silas Bartha <silas@exvacuum.dev>
2024-02-21 02:25:11 -0500

    Added New Comments

--- OPVS-FRIXORIVM/Assets/Scripts/Player/PlayerMovement.cs
+++ OPVS-FRIXORIVM/Assets/Scripts/Player/PlayerMovement.cs
@@ -19,6 +19,13 @@ public class PlayerMovement: PlayerController
     ///     Base player movement speed
     /// </summary>
     [SerializeField] private float _baseWalkSpeed = 2.0f;
+
+    [SerializeField]
+    private float _accelerationSpeed = 2.0f;
+
+    [SerializeField]
+    private float _decelerationSpeed = 4.0f;
+    
     
     /// <summary>
     ///     Velocity impulse on jump
@@ -40,9 +47,12 @@ public class PlayerMovement: PlayerController
     ///     Type of device controlling this player
     /// </summary>
     private string _deviceClass;
-
+    
+    private bool _knockedOut;
     private Camera _camera;
-
+    private Quaternion _orientation;
+    private Vector2 _walkValue;
+    
     protected override void Awake()
     {
         _characterController = GetComponent<CharacterController>();
@@ -55,17 +65,75 @@ public class PlayerMovement: PlayerController
 
     private void Update()
     {
-        if (!_characterController.enabled) return;
-        // Apply gravity
-        _velocity.y -= 9.81f * Time.deltaTime;
-        if (_characterController.isGrounded) _velocity.y = 0;
+        UpdateWalk();
         
+        if (_knockedOut) return;
+        UpdateLook();
+        ApplyGravity();
+        ApplyMovement();   
+    }
+
+    /// <summary>
+    ///     Applies gravity
+    /// </summary>
+    private void ApplyGravity()
+    {
+        _velocity.y -= 9.81f * Time.deltaTime;
+    }
+
+    /// <summary>
+    ///     Update horizontal movement (acceleration, deceleration, etc.)
+    /// </summary>
+    private void UpdateWalk()
+    {
+        // Stop moving character controller if knocked out (rigidbody will continue on its own)
+        if (_knockedOut)
+        {
+            _velocity = Vector3.zero;
+            return;
+        }
+        
+        var playerMoveDir = new Vector2(_velocity.x, _velocity.z).normalized;
+        if (_walkValue == Vector2.zero)
+        {
+            if (_velocity.x != 0.0f || _velocity.z != 0.0f)
+            {
+                
+                _velocity.x -= playerMoveDir.x * _decelerationSpeed * Time.deltaTime;
+                _velocity.z -= playerMoveDir.y * _decelerationSpeed * Time.deltaTime;
+                return;
+            }
+        }
+
+        var walkVelocity = Vector2.ClampMagnitude(new Vector2(
+            _velocity.x + _walkValue.x * _accelerationSpeed * Time.deltaTime,
+            _velocity.z + _walkValue.y * _accelerationSpeed * Time.deltaTime
+        ), _baseWalkSpeed);
+        _velocity.x = walkVelocity.x;
+        _velocity.z = walkVelocity.y;
+    }
+
+    /// <summary>
+    ///     Updates player rotation from look direction
+    /// </summary>
+    private void UpdateLook()
+    {
+        transform.rotation = _orientation;
+    }
+
+    /// <summary>
+    ///     Apply movement to player controller
+    /// </summary>
+    private void ApplyMovement()
+    {
        _characterController.Move(_velocity * Time.deltaTime);
+        if (_characterController.isGrounded) _velocity.y = 0;
     }
 
     private void FixedUpdate()
     {
-        Debug.DrawLine(transform.position, transform.position + transform.forward * 3.0f, Color.red);
+        var position = transform.position;
+        Debug.DrawLine(position, position + transform.forward * 3.0f, Color.red);
     }
 
     /// <summary>
@@ -73,18 +141,13 @@ public class PlayerMovement: PlayerController
     /// </summary>
     private void OnJump(InputValue value)
     {
-        if (value.Get<float>() == 1f)
-        {
+        if (!value.isPressed)
+            return;
+        _velocity.y = _jumpVelocity;
 
-            if (_characterController.isGrounded)
-            {
-                Debug.Log("Boing");
-                _velocity.y = _jumpVelocity;
-            }
-            else
-            {
-                StartCoroutine(KnockOut());
-            }
+        if (!_characterController.isGrounded && !_knockedOut)
+        {
+            StartCoroutine(KnockOut());
         }
     }
 
@@ -93,9 +156,7 @@ public class PlayerMovement: PlayerController
     /// </summary>
     private void OnWalk(InputValue value)
     {
-        var moveValue = value.Get<Vector2>();
-        _velocity.x = moveValue.x * _baseWalkSpeed;
-        _velocity.z = moveValue.y * _baseWalkSpeed;
+        _walkValue = value.Get<Vector2>();
     }
 
     /// <summary>
@@ -103,21 +164,26 @@ public class PlayerMovement: PlayerController
     /// </summary>
     private void OnLook(InputValue value)
     {
+        if (_knockedOut) return;
         var vecValue = value.Get<Vector2>();
+        if (vecValue == Vector2.zero) return;
         if (_deviceClass == "Keyboard")
         {
             vecValue -= (Vector2)_camera.WorldToScreenPoint(transform.position);
         }
-        transform.rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * Mathf.Atan2(-vecValue.y, vecValue.x) + 90f, Vector3.up);
+        _orientation = Quaternion.AngleAxis(Mathf.Rad2Deg * Mathf.Atan2(-vecValue.y, vecValue.x) + 90f, Vector3.up);
     }
 
     private IEnumerator KnockOut()
     {
         _characterController.enabled = false;
         _rigidbody.isKinematic = false;
+        _rigidbody.velocity = _velocity;
+        _knockedOut = true;
 
         yield return new WaitForSeconds(_knockoutTime);
 
+        _knockedOut = false;
         _characterController.enabled = true;
         _rigidbody.isKinematic = true;
     }
