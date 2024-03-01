7301dbe0018a1690d3ae7bec4a9d17b3f60a741b
Ethan Major <ethanmajor9@gmail.com>
2024-03-01 15:58:22 -0500

    Added game settings and game manager

---
+++ OPVS-FRIXORIVM/Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameSettings gameSettings;
+    
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- OPVS-FRIXORIVM/Assets/Scripts/Lobby/LobbyManager.cs
+++ OPVS-FRIXORIVM/Assets/Scripts/Lobby/LobbyManager.cs
@@ -3,22 +3,32 @@ using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Lobby
 {
     public class LobbyUIManager : MonoBehaviour
     {
+        [Header("UI Elements")]
         [SerializeField] private TMP_Text countdownText;
         [SerializeField] private GameObject countdownPanel;
-        [SerializeField] private float countdownTimer = 3.0f;
-        [SerializeField] private PlayerManager playerManager;
+        [SerializeField] private GameObject playersNotReadyPanel;
+        [SerializeField] private TMP_Dropdown roundTimerDropdown;
+        [SerializeField] private TMP_Dropdown numberOfRoundsDropdown;
+        [SerializeField] private Toggle audienceEnabledToggle;
 
-        
+        [Header("Countdown Settings")]
+        [SerializeField] private float countdownTimer = 3.0f;
+
+        [Header("References")]
+        [SerializeField] private PlayerManager playerManager;
+        [SerializeField] private GameSettings gameSettings;
+
         private bool _cancelled;
         private Coroutine _countdownCoroutine;
+        private const string GameScenePath = "Assets/Scenes/GameScene.unity";
 
-        public void Awake()
+        private void Awake()
         {
             countdownPanel.SetActive(false);
             _cancelled = false;
@@ -27,12 +37,17 @@ namespace Lobby
         public void OnStartClick()
         {
             if (!playerManager.AllPlayersReady())
+            {
+                playersNotReadyPanel.SetActive(true);
                 return;
+            }
+            
             countdownPanel.SetActive(true);
             _cancelled = false; 
         
             if (_countdownCoroutine != null)
                 StopCoroutine(_countdownCoroutine);
+            
             _countdownCoroutine = StartCoroutine(StartCountdown());
         }
 
@@ -48,12 +63,23 @@ namespace Lobby
 
             countdownPanel.SetActive(false); 
             _cancelled = false;
+            UpdateGameSettings();
+            SceneManager.LoadScene(GameScenePath, LoadSceneMode.Single);
         }
 
-        public void OnCancelClick()
+        private void UpdateGameSettings()
+        {
+            var roundCount = int.Parse(numberOfRoundsDropdown.options[numberOfRoundsDropdown.value].text);
+            var audienceEnabled = audienceEnabledToggle.isOn;
+            var roundTimeLimit = int.Parse(roundTimerDropdown.options[roundTimerDropdown.value].text);
+            
+            gameSettings.UpdateGameSettings(roundCount, roundTimeLimit, audienceEnabled);
+        }
+
+        public void OnCancelClick(GameObject menu)
         {
             _cancelled = true;
-            countdownPanel.SetActive(false); 
+            menu.SetActive(false); 
         }
     }
 }
